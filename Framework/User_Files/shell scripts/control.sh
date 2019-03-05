67f1ce6a62c753d6dba87fc914311b15798f3649
TRSummers <42047622+TRSummers@users.noreply.github.com>
2019-03-05 15:04:10 -0800

    Add files via upload

---
+++ Framework/User_Files/shell scripts/control.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+clear
+
+	x=1; j=1
+	yorn='n'
+	
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	echo
+	echo "Run a test from the remote Gatling host against a cloud-based System Under Test."
+	echo
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	echo 
+	echo "Use this script to run a test from the Gatling host against an existing application instance."
+	echo "Currently, you can run with defaults or you can modify various runtime parameters."
+	echo 
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	echo 
+	echo "Defaluts that can be overridden re as follows:"
+	echo
+	echo
+	echo "Number of iterations.............10"
+	echo "Ramp up time.................... 20 (seconds)"
+	echo "Number of users for SIMULATION_1..5"
+	echo "Number of users for SIMULATION_2..5"
+	echo "Number of users for SIMULATION_3. 2"
+	echo "Number of users for SIMULATION_2. 2"
+	echo 
+	echo "Defaults that currently cannot be overridden:"
+	echo
+	echo "HOST..............................ec2-54-74-168-205.eu-west-1.compute.amazonaws.com:8080"
+	echo "GATLING_HOME....................../users/gatling-3.0.3/"
+	echo 
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	read -p "Hit <enter> to continue."
+	echo
+	clear
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	echo 
+	echo "Defaluts that can be overridden re as follows:"
+	echo
+	echo
+	echo "Number of iterations.............10"
+	echo "Ramp up time.................... 20 (seconds)"
+	echo "Number of users for SIMULATION_1..5"
+	echo "Number of users for SIMULATION_2..5"
+	echo "Number of users for SIMULATION_3. 2"
+	echo "Number of users for SIMULATION_2. 2"
+	echo 
+	echo "Defaults that currently cannot be overridden:"
+	echo
+	echo "HOST..............................ec2-54-74-168-205.eu-west-1.compute.amazonaws.com:8080"
+	echo "GATLING_HOME....................../users/gatling-3.0.3/"
+	echo 
+	echo "=============================================================================================="
+	echo "=============================================================================================="
+	echo "When prompted, enter D for defaults, as defined above or"
+	echo "enter O and then enter override values as prompted. Case doesn't matter."
+	echo
+	echo "Defaults............D"
+	echo "You will not prompted for further inupt."
+	echo
+	echo "Override vars......O"
+	echo "Note: If overriding defaults, you are required to enter all values."
+	echo
+	echo "Enter these operands when prompted:"
+	echo "Iteratons........i"
+	echo "Ramp.............r"
+	echo "Simulation(s).......1,2,3,4"
+	echo
+	echo "=============================================================================================="
+	read -p "Enter D (Defult) or O (Override): " var
+
+  	case  $var  in
+		[D,d])       
+			clear
+			echo "=============================================================================================="
+			echo
+			echo "Running with Default runtime values."
+			echo
+			echo "Iterations............10"
+			echo "Ramp up time..........20 (Seconds)"
+			echo "Simulation_1 Users....5"
+			echo "Simulation_2 Users....5"
+			echo "Simulation_3 Users....2"
+			echo "Simulation_4 Users....2"
+			echo 
+			echo "=============================================================================================="
+
+			ssh -i "~/.ssh/tsummers-ec2.pem" root@ec2-54-155-136-115.eu-west-1.compute.amazonaws.com /usr/gatling-3.0.3/user-files/simulations/gat.sh
+
+#			break
+                    ;;
+
+		[O,o])
+			clear
+			while [ "$yorn" == "n" ];
+			  do
+				echo "=============================================================================================="
+				echo
+				echo "How Many Iterations?"
+				read -p  'Enter number: ' iterations 
+				echo "Ramp up time"
+				read -p  'Enter number: ' ramp
+				echo "Users for Simulation 1"
+				read -p  'Enter number: ' s1
+				echo "Users for Simulation 2"
+				read -p  'Enter number: ' s2
+				echo "Users for Simulation 3"
+				read -p  'Enter number: ' s3
+				echo "Users for Simulation 4"
+				read -p  'Enter number: ' s4
+				clear
+				echo "==================================================="
+				echo
+				echo "Running with the following controls"
+				echo
+				echo "Iterations..............$iterations"
+				echo "Ramp up time............$ramp (seconds)"
+				echo "Simulation 1 Users......$s1"
+				echo "Simulation 2 Users......$s2"
+				echo "Simulation 3 Users......$s3"
+				echo "Simulation 4 Users......$s4"
+				echo
+				echo "==================================================="
+				read -p "Run with these overrides? Y or N: " yorn
+
+			done
+			echo "Running..."
+			~/.ssh/runremote.sh $iterations $ramp $s1 $s2 $s3 $s4
+		    ;;
+		esac 
+
+
+#	done
---
+++ Framework/User_Files/shell scripts/gat.sh
@@ -0,0 +1,94 @@
+#
+# Can be used from any directory to start a gatling job
+# Sets defaults for command-line parameters
+#
+
+
+	while getopts "g:s:r:i:h:n:" opt
+   	do
+     		case $opt in
+        		g ) GATLING_HOME=$OPTARG
+				;;
+        		s ) SIMULATIONS=$OPTARG
+				;;
+        		r ) RAMP=$OPTARG
+				;;
+        		i ) ITERATIONS=$OPTARG
+				;;
+        		h ) HOST=$OPTARG
+				;;
+        		n ) SCENARIO=$OPTARG
+				;;
+     		esac
+	done
+
+	let i=1
+	
+	for SIMULATION in $SIMULATIONS;do
+		let SIMULATION_$i=$SIMULATION
+      		echo $SIMULATION
+		let i+=1
+	done
+
+	if [[ -z $SIMULATION_4 ]]; then
+		let SIMULATION_4=0 
+	fi
+
+	if [[ -z $GATLING_HOME ]]; then
+		GATLING_HOME='/Applications/gatling-charts-highcharts-bundle-3.0.3/'
+	fi
+
+	if [[ -z $SIMULATIONS ]]; then
+		SIMULATION_1=5
+		SIMULATION_2=5
+		SIMULATION_3=2
+		SIMULATION_4=2
+	fi
+
+	if [[ -z $ITERATIONS ]]; then
+		ITERATIONS=10
+	fi
+
+	if [[ -z $RAMP ]]; then
+		RAMP=20
+	fi
+
+	if [[ -z $HOST ]]; then
+		HOST='ec2-54-74-168-205.eu-west-1.compute.amazonaws.com:8080'
+	fi
+
+	if [[ -z $SCENARIO ]]; then
+		SCENARIO=DM.Framework.Sim
+	fi
+
+	export SCENARIO
+	export HOST
+	export ITERATIONS
+	export RAMP
+	export SIMULATION_1
+	export SIMULATION_2
+	export SIMULATION_3
+	export SIMULATION_4
+	export GATLING_HOME
+
+# RESULT_LABEL will be added to the Gatling Report. It will aid in the identification of the test
+	RESULT_LABEL=$SCENARIO-SIMULATION_1_$SIMULATION_1:SIMULATION_2_$SIMULATION_2:SIMULATION_3_$SIMULATION_3:SIMULATION_4_$SIMULATION_4:Iterations_$ITERATIONS
+
+# ON is a Gatling parameter. It will indicate the ouptput directory, making it much easier to tell which test it is
+	ON=`date +%m-%d-%y-%H:%M:%S`
+
+	echo "========================"
+	echo "SCENARIO:" $SCENARIO
+	echo "HOST:" $HOST
+	echo "ITERATIONS:" $ITERATIONS
+	echo "RAMP:" $RAMP
+	echo "SIMULATION_1:" $SIMULATION_1
+	echo "SIMULATION_2:" $SIMULATION_2
+	echo "SIMULATION_3:" $SIMULATION_3
+	echo "SIMULATION_4:" $SIMULATION_4
+	echo "GATLING_HOME" $GATLING_HOME
+	echo "Output Directory:" $RF
+	echo "RESULT_LABEL" $RESULT_LABEL
+	echo "========================"
+
+JAVA_OPTS="-DHOST=$HOST -DITERATIONS=$ITERATIONS -DRAMP=$RAMP -DSIMULATION_1=$SIMULATION_1 -DSIMULATION_2=$SIMULATION_2 -DSIMULATION_3=$SIMULATION_3 -DSIMULATION_4=$SIMULATION_4" $GATLING_HOME/bin/gatling.sh  -s $SCENARIO -rd $RESULT_LABEL -rf $GATLING_HOME/results/$ON
